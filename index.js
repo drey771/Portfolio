977ad54a3cce5c80cf96afea68dbcddf3209eed0
Babatunde damilare daniel <bdamilare3@gmail.com>
2024-01-14 22:40:38 +0100

    Initial commit
    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert(`Welcome to Drey Portfolio `);
+
+let header = document.querySelector("#header");
+let profile = document.querySelector("displayMain");
+
+const showMain = () => {
+  header.style.display = "flex";
+};
+
+setTimeout(showMain, 3000);
